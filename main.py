e3c1f9fb698fd347cc1007a81416d1a6b34deab0
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-11-25 23:04:55 +0200

    Progress bar

--- main.py
+++ main.py
@@ -1,104 +1,80 @@
 from tkinter import *
-from tkinter import messagebox ,ttk
-from pytube import *
+import os
+from moviepy.editor import *
+from tkinter import messagebox, ttk
+from pytube import YouTube
+import _thread
 
+storagePath = r"/home/mohammad/Downloads/video_Downloader"
+
+# main window
 root = Tk()
-root.title('Viedo Downloader')
-root.geometry("800x600")
-root.resizable(False, False)
-root.config(background='#D5DBDB')
-root.iconbitmap('logo.ico')
+root.title("Youtube Downloader")
+root.geometry('700x400')
+root.resizable(0, 0)
+def show_progress_bar(stream,chunk,bytes_remaining):
+    progress = int(((stream.filesize - bytes_remaining)/stream.filesize)*100)
+    bar["value"] = progress
 
-# ------- title ------
-title = Label(root, text='Welcome To Your Video Downloader', font=('Courier', 25), bg='black', fg='white')
-title.pack(fill=X)
-
-title2 = Label(root, text='https://youtu.be/adJFT6_j9Uk?list=LL', font=('Courier', 25), bg='white', fg='white')
-title2.pack(fill=X)
-
-# -------- Frame ------
-# fr1 = Frame(root, width='300', height = '350' , bg = 'whitesmoke')
-# fr1.pack(pady = 30)
-# --------- image -------
-
-# photo = PhotoImage(file = 'Prey-logo-1-icon.png')
-# panel = Label(root, image = photo)
-# panel.pack()
-
-# --------- Lable ---------
-# Url_lable = Label(root, text="URL   :", font=('Courier', 20))
-# Url_lable.place(x=10, y=110)
-
-list_Button = Button(root, text="PlayList", bg="green", font=('Courier', 25))
-list_Button.place(x=610, y=70, width=180, height=37)
+# download window
 
 
+def download():
+    quality = ytbchoices.get()
+    url = link.get()
+    if len(url) > 0:
+        msg['text'] = 'Extracting video from youtube...'
+        ytb_url = YouTube(url, on_progress_callback=show_progress_bar)
+        video = ytb_url.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+        audio = ytb_url.streams.filter(only_audio=True).first()
 
 
-# ------- Entry _____
-url_path = Entry(root)
-url_path.place(x=40, y=240, width=500, height=37)
-
-def init_cal():
-    calc = Toplevel()
-    calc.title('Viedo Downloader')
-    calc.geometry("800x600")
-    calc.resizable(False, False)
-    calc.config(background='#D5DBDB')
-    calc.iconbitmap('logo.ico')
-
-    root.withdraw()
-    path = str(url_path.get())
-    yt = YouTube(path)
-    print(yt.title)
-    video_lable = Label(calc, text=f'{yt.title}')
-    video_lable.pack()
-
-    video_Quality = yt.streams.filter(progressive=True)
-    audio_Quality = yt.streams.filter(only_audio=True)
-    # if video_Quality and audio_Quality:
-    #     for stream in video_Quality:
-    #         stream_lable = Label(calc, text=stream)
-    #         stream_lable.pack()
-    #
-    #     for audio in audio_Quality:
-    #         audio_lable = Label(calc, text=audio)
-    #         audio_lable.pack()
-
-    res = Label(calc, text='Resolution')
-    res.pack(width=500, height=37)
-
-    resolution =ttk.Combobox(calc, textvariable=video_Quality)
-    resolution.bind('<<ComboboxSelected>>', callback)
-
-    variable = StringVar(calc)
-    variable.set("one")  # default value
-    w = OptionMenu(calc, variable, "one", "two", "three")
-    w.pack(w)
-        # stream = yt.streams.get_by_itag(22)
-        # stream.download(output_path=r'C:\Users\STUDENT\Documents\Downloads\Vide_Downloader')
-
-
-
-    def on_closing():
-        calc.destroy()
-        root.deiconify()
-    calc.protocol("WM_DELETE_WINDOW", on_closing)
-
-# https://youtu.be/adJFT6_j9Uk?list=LL
-
-def handler():
-    if url_path.get() :
-        myButton = Button(root, text="Download", command=init_cal, bg="green", font=('Courier', 25))
-        myButton.place(x=300, y=350, width=200, height=37)
-
+        msg["text"] = "Downloding" + ytb_url.title
+        if quality == choices[0]:
+            video.last().download(storagePath)
+        elif quality == choices[1]:
+            video.first().download(storagePath)
+        else:
+            print(ytb_url.title)
+            audio.download(storagePath)
+            # video = "/home/mohammad/Downloads/video_Downloader/PhysicsExplainedinTenSeconds(Announcement).mp4"
+            # audio1 = "test.mp3"
+            # videoClip = VideoClip(video)
+            # audioClip = videoClip.audio
+            # audioClip.write_audiofile(audio1)
+        msg["text"] = "Downloaded Successfully"
+        messagebox.showinfo("Download info", "Downloaded Successfully and saved to\n" + storagePath)
     else:
-        messagebox.showwarning(title='Error', message='Please Enter The Video URL ')
+        urlErr["text"] = "Please Enter the URL"
 
 
-Enter_Button = Button(root, text="Enter", bg="green", font=('Courier', 25), command=handler)
-Enter_Button.place(x=560, y=240, width=140, height=37)
+# Heading
+Label(root, text="Youtube Video Downloader", font="arial 20 bold").pack()
+
+# url entry
+Label(root, text="Paste the link here", font='arial 15 bold').pack()
+link = StringVar(root)
+link_entry = Entry(root, textvariable=link, width=70).pack()
+
+# url error message
+urlErr = Label(root, font='arial 12', fg = 'red')
+urlErr.pack()
+
+# quality
+Label(root, text="select the quality of video", font='arial 12 bold').pack(pady=10)
+choices = ["low", "high","audio"]
+ytbchoices = ttk.Combobox(root, values=choices)
+ytbchoices.pack()
+
+#progress bar
+bar = ttk.Progressbar(root, length=300)
+bar.pack(pady=10)
+
+# msg
+msg = Label(root, font= "arial 12", fg="green")
+msg.pack()
+
+# download button
+Button(root, text="DOWNLOAD", fg="white", bg="#E21717", width=17, height=2, command= lambda : _thread.start_new_thread(download,())).pack(pady=10)
 
 root.mainloop()
-
-
